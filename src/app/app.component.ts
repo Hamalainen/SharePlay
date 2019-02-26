beb1b67b154a2e805dee39a5237b3e0e3f6ca71c
Thomas Hämäläien <Hamalainen@github.com>
2019-02-26 23:32:19 +0100

    starting with youtubeplayer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlayerIndex } from '@angular/core/src/render3/interfaces/player';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SharePlay';
+
+
+  player: YT.Player;
+  private id: string = 'qDuKsiwS5xw';
+
+  savePlayer(player) {
+    this.player = player;
+    console.log('player instance', player);
+  }
+  onStateChange(event) {
+    console.log(this.player.getVideoUrl.toString);
+    console.log("spellista "+this.player.getPlaylist);
+    if(this.player.getPlaylist == null){
+      console.log("laddar lista");
+      this.player.loadPlaylist;
+    }
+    if(event.data == 0){
+      this.player.nextVideo;
+      this.player.playVideo;
+      console.log("spelar")
+    }
+    console.log('player state', event.data);
+  }
 }
