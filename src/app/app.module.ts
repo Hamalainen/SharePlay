4867e937d9ed009c41a4a547702084f7deb625c1
Thomas Hämäläien <Hamalainen@github.com>
2019-02-27 22:58:09 +0100

    added some declarations in app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,28 @@ import { YoutubePlayerService } from './shared/services/youtube-player.service';
 import { PlaylistStoreService } from './shared/services/playlist-store.service';
 import { NotificationService } from './shared/services/notification.service';
 import { BrowserNotificationService } from './shared/services/browser-notification.service';
-
+//Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {PlayerComponent} from './main/player/player.component';
 import { MainComponent } from './main//main.component'
+// Pipes
+import { VideoDurationPipe } from './shared/pipes/video-duration.pipe';
+import { VideoLikesViewsPipe } from './shared/pipes/video-likes-views.pipe';
+import { VideoNamePipe } from './shared/pipes/video-name.pipe';
+import { LazyScrollDirective } from './shared/directives/lazy-scroll/lazy-scroll.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerComponent,
-    MainComponent
+    MainComponent,
+
+    VideoDurationPipe,
+    VideoLikesViewsPipe,
+    VideoNamePipe,
+    LazyScrollDirective
   ],
   imports: [
     BrowserModule,
