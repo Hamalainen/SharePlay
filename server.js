78305a8c8e9a18aab075a32903c6cc0aaad91c85
Thomas Hämäläien <Hamalainen@github.com>
2019-08-20 09:09:36 +0200

    merged socketserver and angular app

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-
-'use strict';
-
-//Install express server
-//const express = require('express');
-//const path = require('path');
-
-//const app = express();
-
-// Serve only the static files form the dist directory
-//app.use(express.static(__dirname + '/dist/SharePlay'));
-
-//app.get('/*', function(req,res) {
-    
-//res.sendFile(path.join(__dirname+'/dist/SharePlay/index.html'));
-//});
-//const clientPort = process.env.PORT || 8080;
-
-// Start the app by listening on the default Heroku port
-//app.listen(clientPort);
-
-//console.log("clientport: " + clientPort);
-
-
-
-//nedan är socketservern
-/////////////////////////////////////////////////
-
-
 'use strict';
 
 const express = require("express");
