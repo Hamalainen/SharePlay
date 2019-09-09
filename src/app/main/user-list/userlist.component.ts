159a5369364fde9af9cd3f12d7fdc7494c89eed2
Thomas Hamalainen <Hamalainen@github.com>
2019-09-09 13:02:00 +0200

    trying to get ip from client

--- src/app/main/user-list/userlist.component.ts
+++ src/app/main/user-list/userlist.component.ts
@@ -3,6 +3,7 @@ import { UserNameService } from '../../shared/services/user-name.service';
 import { SyncService } from '../../shared/services/sync.service';
 import { MainComponent } from '../main.component';
 import { IpLookupService } from '../../shared/services/ip-lookup.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-userlist',
@@ -17,7 +18,8 @@ export class UserlistComponent implements OnInit {
     private userNameService: UserNameService,
     private syncService: SyncService,
     private ipLookupService: IpLookupService,
-    public mainComponent: MainComponent
+    public mainComponent: MainComponent,
+    private http: HttpClient
   ) { }
 
   ngOnInit() {
@@ -26,6 +28,10 @@ export class UserlistComponent implements OnInit {
       this.syncService.addUserName(res['name'] + " " + res['surname']);
     });
 
+    this.http.get<{ip:string}>('https://jsonip.com')
+    .subscribe( data => {
+      console.log('th data', data);
+    })
     this.ipLookupService.ipLookUp('ip').subscribe(res =>{
       console.log(res);
     });
