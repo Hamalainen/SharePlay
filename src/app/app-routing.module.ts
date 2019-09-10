c088d6beff5bc3a31f1d16e30299d71f601c34f6
Thomas Hamalainen <Hamalainen@github.com>
2019-09-10 09:50:04 +0200

    changed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MainComponent} from './main/main.component'
+import { MainComponent } from './main/main.component'
 import { SocketStatusComponent } from './main/socket-status/socket-status.component'
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: ':room', component: MainComponent },
-  { path: ':room/server', component: SocketStatusComponent }
+  { path: 'server', component: SocketStatusComponent },
+  { path: ':room', component: MainComponent }
 ];
 
 @NgModule({
